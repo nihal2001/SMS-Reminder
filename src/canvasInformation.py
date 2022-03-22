4dec0757a1e0e541159736dca492dd5932fb30fd
nihal8590 <nihal8590@vt.edu>
2022-03-22 02:14:20 -0400

    Update canvasInformation.py

--- src/canvasInformation.py
+++ src/canvasInformation.py
@@ -6,9 +6,14 @@ Gets information from API and stores it
 from canvasapi import Canvas
 
 # Canvas API URL
-API_URL = "https://example.com"
+API_URL = "https://canvas.vt.edu"
 # Canvas API key
 API_KEY = "4511~vZrrov6HR0ZVZNKkf4IdEOz0U8h5BHuMiPxcOko0z4yIAF7dF67mLMUXfmX4ZSgE"
 
+headers = {'Authorization': 'Bearer %s' % API_KEY}
+
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
+
+myCourses = canvas.get("https://canvas.vt.edu/api/v1/courses", headers=headers, params={'enrollment_state':'active', 'include':['term']}).json()
+print(myCourses)
